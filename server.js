70fdaf1c5985f902bb2da4b846505c5e84bd4601
Amirah Kabir <maiza1497@gmail.com>
2018-11-08 02:58:37 +0000

    html file added to simple server

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 var http = require('http');
+var fs= require('fs');
+
   var server = http.createServer(function(req, res){
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end("Hello World\n");
+    res.setHeader('Content-Type', 'text/html');
+    fs.readFile('index.html', function (err, data){
+      if (err){
+        return console.log("File read error");
+      }  
+      res.end(data);
+    });
+    //res.end("Hello World\n");
   });
-  server.listen(process.env.PORT, process.env.IP, function(){
+  server.listen(process.env.PORT, /* || 3000 (for local host)*/ process.env.IP, /* || 'localhost' */ function(){
     console.log('Server running');
   });
