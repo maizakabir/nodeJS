a26fb83c9794dfbc2fbe02ec8f78dd5262fe7859
Amirah Kabir <maiza1497@gmail.com>
2018-11-12 03:06:21 +0000

    .gitignore added

--- server.js
+++ server.js
@@ -1,17 +1,34 @@
 var http = require('http');
-var fs= require('fs');
+var  express = require('express');
+var app = express();
+var server = http.Server(app);
 
-  var server = http.createServer(function(req, res){
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    fs.readFile('index.html', function (err, data){
-      if (err){
-        return console.log("File read error");
-      }  
-      res.end(data);
-    });
-    //res.end("Hello World\n");
-  });
-  server.listen(process.env.PORT, /* || 3000 (for local host)*/ process.env.IP, /* || 'localhost' */ function(){
-    console.log('Server running');
-  });
+app.get('/', function(request, response){ /*'/' means root route*/
+  response.sendFile(__dirname+'/index.html');   /*__dirname is the root address*/
+});
+
+server.listen(process.env.PORT, /* || 3000 (for local host)*/ process.env.IP, /* || 'localhost' */ function(){
+  console.log('Server running');
+});
+
+  
+// var fs= require('fs');
+
+//   var server = http.createServer(function(req, res){
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/html');
+//     fs.readFile('index.html', function (err, data){
+//       if (err){
+//         return console.log("File read error");
+//       }  
+//       res.end(data);
+//     });
+//     //res.end("Hello World\n");
+//   });
+  // server.listen(process.env.PORT, /* || 3000 (for local host)*/ process.env.IP, /* || 'localhost' */ function(){
+  //   console.log('Server running');
+  // });
+
+
+
+
